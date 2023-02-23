a6c880eed1ac5019288f0860fec25b9603631e59
Guru Raghupathy <guru@doit-intl.com>
2023-02-23 19:32:06 +0000

    Spring boot boiler plate

---
+++ src/main/java/com/doit/flooid/rest/FlooidRestApplication.java
@@ -0,0 +1,13 @@
+package com.doit.flooid.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlooidRestApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(FlooidRestApplication.class, args);
+	}
+
+}
